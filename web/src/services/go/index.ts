b701e3eadfc0c77ec5f1c1e0a78c57cac900a434
x1unix <denis0051@gmail.com>
2020-12-26 07:08:49 +0200

    wasm: simplify global wrapper

--- web/src/services/go/index.ts
+++ web/src/services/go/index.ts
@@ -1,6 +1,5 @@
 import { FileSystemWrapper } from './fs';
 import ProcessStub from './process';
-import { Global } from './go';
 import { Go } from './wasm_exec';
 import {StdioWrapper, ConsoleLogger} from './stdio';
 
@@ -31,19 +30,13 @@ export const bootstrapGo = (logger: ConsoleLogger) => {
     wrapper = new StdioWrapper(logger);
 
     // global overlay
-    const mocks = {
+    const globalWrapper = {
         fs: new FileSystemWrapper(wrapper.stdoutPipe, wrapper.stderrPipe),
         process: ProcessStub,
         Go: Go,
     };
 
     // Wrap global object to make it accessible to Go's wasm bridge
-    const globalWrapper = new Proxy<Global>(window as any, {
-        has: (obj, prop) => prop in obj || prop in mocks,
-        get: (obj, prop) => {
-            console.log('go: get %s', prop);
-            return prop in obj ? obj[prop] : mocks[prop]
-        }
-    });
+    Object.setPrototypeOf(globalWrapper, window);
     instance = new Go(globalWrapper);
 };
